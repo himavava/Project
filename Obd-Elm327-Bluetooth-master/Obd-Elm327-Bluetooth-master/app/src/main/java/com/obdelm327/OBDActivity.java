2a997e85b499f15552abcc2cad3c0c790245adea
himavava <himavava@gmail.com>
2019-03-26 16:51:56 +0530

    Alert to driver when overspeed

--- Obd-Elm327-Bluetooth-master/Obd-Elm327-Bluetooth-master/app/src/main/java/com/obdelm327/OBDActivity.java
+++ Obd-Elm327-Bluetooth-master/Obd-Elm327-Bluetooth-master/app/src/main/java/com/obdelm327/OBDActivity.java
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2009 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package com.obdelm327;
 import java.util.ArrayList;
@@ -442,7 +427,7 @@ public class OBDActivity extends Activity {
 		 Load.setText("0 %");
 	     Fuel.setText("0 / 0 Mileage");
 	     Volt.setText("0 V");
-	     Temp.setText("0 C°");
+	     Temp.setText("0 C\B0");
 	     avgconsumption.clear();
 	     initialized=false;
 	     speed.setTargetValue(200);
@@ -780,7 +765,7 @@ public class OBDActivity extends Activity {
 	            else if (tmpmsg.equals("4105")){
   	            	 int tempC = obdval - 40;
   	            	 currenttemp=tempC;
-  	            	 Temp.setText(Integer.toString(tempC) + " C°");	
+  	            	 Temp.setText(Integer.toString(tempC) + " C\B0");	
   	             }
 	            else if (tmpmsg.contains("4104"))
   	             {		            	
